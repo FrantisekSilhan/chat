d071db61206df42e26858730d6bb79c2dfae02d3
František Šilhán <frantisek@slhn.cz>
2024-01-01 22:10:21 +0100

    add .env

--- src/sessionMiddleware.js
+++ src/sessionMiddleware.js
@@ -1,13 +1,14 @@
 // sessionMiddleware.js
 const session = require("express-session");
 const SQLiteStore = require("connect-sqlite3")(session);
+require("dotenv").config();
 
 const sessionMiddleware = session({
   store: new SQLiteStore({
     db: "sessions.db",
     concurrentDB: true,
   }),
-  secret: "your-secret-key",
+  secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
   cookie: {
