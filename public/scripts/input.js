3c7c9d1e8d697002a818c6c8a80c0401562ad201
František Šilhán <frantisek@slhn.cz>
2024-01-02 03:12:04 +0100

    fix chat padding

--- public/scripts/input.js
+++ public/scripts/input.js
@@ -1,11 +1,13 @@
 const textarea = document.getElementById("textarea");
 const chatButton = document.getElementById("send");
+const body = document.getElementsByTagName("body")[0];
 
 if (textarea !== undefined) {
+
   textarea.addEventListener("input", e => {
     const inputText = e.target.value;
     const lines = inputText.split("\n").length;
-    textarea.style.height = (lines * 1.2) + "em";
+    body.style.setProperty("--chat-height-input", (Math.ceil((lines * parseFloat(getComputedStyle(body).getPropertyValue('--chat-line-height')))*10)/10) + "em");
   });
 
   textarea.addEventListener("keydown", e => {
@@ -13,6 +15,12 @@ if (textarea !== undefined) {
       e.preventDefault();
       console.log("pressed")
       chatButton.dispatchEvent(new Event("click", {}));
+    }
+
+    if (e.key === "Enter" && e.shiftKey) {
+      setTimeout(() => {
+        window.scrollTo(0, document.body.scrollHeight);
+      }, 10);
     }
   })
 }
