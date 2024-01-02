e864ef3597620038dcf5ef34ca143a2b0d22b2c2
František Šilhán <frantisek@slhn.cz>
2024-01-02 01:54:40 +0100

    change multiline key

--- public/scripts/palette.js
+++ public/scripts/palette.js
@@ -7,10 +7,10 @@ function applyRandomPalette() {
 
   const prefersLightMode = window.matchMedia("(prefers-color-scheme: light)").matches;
 
-  document.documentElement.style.setProperty('--text', `${hue} 22% ${prefersLightMode ? 9 : 91}%`);
-  document.documentElement.style.setProperty('--background', `${hue} 20% ${prefersLightMode ? 96 : 4}%`);
-  document.documentElement.style.setProperty('--primary', `${hue} 20% ${prefersLightMode ? 30 : 70}%`);
-  document.documentElement.style.setProperty('--secondary', `${hue} 20% ${prefersLightMode ? 48 : 52}%`);
+  document.documentElement.style.setProperty("--text", `${hue} 22% ${prefersLightMode ? 9 : 91}%`);
+  document.documentElement.style.setProperty("--background", `${hue} 20% ${prefersLightMode ? 96 : 4}%`);
+  document.documentElement.style.setProperty("--primary", `${hue} 20% ${prefersLightMode ? 30 : 70}%`);
+  document.documentElement.style.setProperty("--secondary", `${hue} 20% ${prefersLightMode ? 48 : 52}%`);
 }
 
 export default applyRandomPalette;
